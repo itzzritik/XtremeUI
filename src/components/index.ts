ee530a8ec144f0498f564084800da8768b7a4c35
Ritik Srivastava <ritik.space@gmail.com>
2023-06-30 15:13:43 +0530

    🎉 Set Vite Packager

---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export type { IHelloWorld } from './base/Hello';
+export { Helloworld } from './base/Hello';
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'node:path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import EsLint from 'vite-plugin-linter'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import * as packageJson from './package.json'
+
+const { EsLinter, linterPlugin } = EsLint
+
+export default defineConfig((configEnv) => ({
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    linterPlugin({
+      include: ['./src}/**/*.{ts,tsx}'],
+      linters: [new EsLinter({ configEnv })],
+    }),
+    dts({
+      include: ['src/components/'],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve('src', 'components/index.ts'),
+      name: 'XtremeUI',
+      formats: ['es', 'umd'],
+      fileName: (format) => `xtreme-ui.${format}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)],
+    },
+  },
+}))
