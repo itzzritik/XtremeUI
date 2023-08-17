701378c045698e2142fe4fc44173e40032c1a4bd
Ritik Srivastava <ritik.space@gmail.com>
2023-08-17 20:55:37 +0530

    ⚡️ Fix types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,13 +1,14 @@
 import { TThemeColor, TThemeScheme } from '#components/context/Theme/type';
 
-declare module '*.scss' {
-	const styles: {
-		readonly [key: string]: string;
-	};
-	export default styles;
-}
-
 declare global {
+
+	declare module '*.scss' {
+		const styles: {
+			readonly [key: string]: string;
+		};
+		export default styles;
+	}
+
 	interface Window {
 		__themeScheme: TThemeScheme;
 		__themeColor: TThemeColor;
