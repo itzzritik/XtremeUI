909c1b58d2873831a719663b8304621c9ee6a15f
Ritik Srivastava <ritik.space@gmail.com>
2023-09-08 04:08:57 +0530

    ⚡️ Upgrade Dependencies

--- src/components/hooks/useDebouncedEffect.ts
+++ src/components/hooks/useDebouncedEffect.ts
@@ -1,9 +1,9 @@
-import { useEffect } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { DependencyList, EffectCallback, useEffect } from 'react';
 
-export const useDebouncedEffect = (effect, deps, delay) => {
+export const useDebouncedEffect = (effect: EffectCallback, deps: DependencyList, delay: number) => {
 	useEffect(() => {
 		const handler = setTimeout(() => effect(), delay);
 		return () => clearTimeout(handler);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [...(deps || []), delay]);
 };
