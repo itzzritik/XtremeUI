3a2f34975a114377c96c4574acbd4f5254236813
Ritik Srivastava <ritik.space@gmail.com>
2023-11-24 02:26:48 +0530

    ⚡️ Add null check for themeController

--- src/utils/helper/themeController.ts
+++ src/utils/helper/themeController.ts
@@ -2,7 +2,7 @@ import { defaultColor, defaultScheme } from '#components/context/Theme/type';
 
 import { STORAGE } from '../constants/commons';
 
-export const themeController = (customDefaultScheme: string, customDefaultColor: string) => `
+export const themeController = (customDefaultScheme?: string, customDefaultColor?: string) => (`
 (function () {
 	function setThemeScheme(newThemeScheme) {
 		document.documentElement.setAttribute("data-theme-scheme", newThemeScheme);
@@ -48,4 +48,4 @@ export const themeController = (customDefaultScheme: string, customDefaultColor:
 	setThemeScheme(themeScheme || "${customDefaultScheme ?? defaultScheme}");
 	setThemeColor(themeColor || "${customDefaultColor ?? defaultColor}");
 })();
-`;
+`);
